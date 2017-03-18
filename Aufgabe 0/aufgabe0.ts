52e0115c133640507e4a7cd95fd7356e6299483e
Jonas <Jonas@LAPTOP-8R0JUJBT>
2017-03-18 10:35:43 +0100

    CSS und TS überarbeitet

--- Aufgabe 0/aufgabe0.ts
+++ Aufgabe 0/aufgabe0.ts
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var getname:string = prompt("Bitte geben Sie in das folgende Feld Ihren Namen ein.");
     var welcome:string = "Hallo";
-    document.body.innerHTML = welcome + getname + ":-)";
+    document.body.innerHTML = welcome + "" + getname + "" + ":-)";
 });
 
