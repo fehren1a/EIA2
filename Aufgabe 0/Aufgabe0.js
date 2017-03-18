bd85367a3f7c613f1ca4cf679ce115922b5bf346
Jonas <Jonas@LAPTOP-8R0JUJBT>
2017-03-18 10:42:48 +0100

    Test

--- Aufgabe 0/Aufgabe0.js
+++ Aufgabe 0/Aufgabe0.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var getname = prompt("Bitte geben Sie in das folgende Feld Ihren Namen ein.");
-    var welcome = "Hallo";
-    document.body.innerHTML = welcome + getname + ":-)";
+    var welcome = "Hallo ";
+    document.body.innerHTML =welcome + getname + " :-)";
 });
 //# sourceMappingURL=aufgabe0.js.map
