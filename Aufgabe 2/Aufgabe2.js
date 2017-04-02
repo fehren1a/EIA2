cd9e62b5d2fe2866c4bc2f914e78ed6419d930aa
Jonas <Jonas@LAPTOP-8R0JUJBT>
2017-04-02 13:20:18 +0200

    Aufgabe 2a Schachbrett

--- Aufgabe 2/Aufgabe2.js
+++ Aufgabe 2/Aufgabe2.js
@@ -1,16 +1,16 @@
 window.onload = function () {
-    let Zeile = 0;
-    let n = 64;
-    let Div;
-    let rice = 1;
+    let zeile = 0; //Deklaration f�r die einzelnen Zeilen
+    let n = 64; //Deklaration f�r die Anzahl der Felder
+    let rice = 1; //Deklaration f�r die Anzahl an Reis
     for (let i = 0; i < n; i++) {
-        let div = document.createElement("div");
-        document.body.appendChild(div);
+        let div = document.createElement("div"); //Div-Element wird erstellt
+        document.body.appendChild(div); //Div-Element wird an das Dokument drangeh�ngt
         if (i % 8 == 0) {
             div.className += " floating";
-            Zeile++;
+            zeile++; //Das Programm soll eine Zeile hochz�hlen
         }
-        if (Zeile % 2 == 1) {
+        if (zeile % 2 == 1) {
+            //Bei einem Feld mit gerader Zahl soll mit schwarz begonnen werden und anschlie�end mit wei�
             if (i % 2 == 0) {
                 div.className += " black";
             }
@@ -18,7 +18,8 @@ window.onload = function () {
                 div.className += " white";
             }
         }
-        if (Zeile % 2 == 0) {
+        if (zeile % 2 == 0) {
+            //Bei einem Feld mit gerader Zahl soll mit wei� begonnen werden und anschlie�end mit schwarz
             if (i % 2 == 0) {
                 div.className += " white";
             }
@@ -26,8 +27,9 @@ window.onload = function () {
                 div.className += " black";
             }
         }
-        rice = Math.pow(2, i);
-        var riceshort = rice.toExponential(4);
+        //Code f�r Anzahl der Reisk�rner
+        rice = Math.pow(2, i); //Reisk�rner werden verdoppelt
+        var riceshort = rice.toExponential(4); //Exponentialschreibweise
         div.textContent = riceshort;
         document.body.appendChild(div);
     }
--- Aufgabe 2/Aufgabe2.ts
+++ Aufgabe 2/Aufgabe2.ts
@@ -1,50 +1,50 @@
 window.onload = function (): void{
     
-let Zeile: number = 0;
-let n: number = 64;
-let Div: string;
-let rice: number = 1;
+let zeile: number = 0; //Deklaration für die einzelnen Zeilen
+let n: number = 64; //Deklaration für die Anzahl der Felder
+let rice: number = 1; //Deklaration für die Anzahl an Reis
     
   for (let i: number = 0; i < n; i++) {
-      let div: HTMLDivElement = document.createElement("div");
-      document.body.appendChild(div);
+      let div: HTMLDivElement = document.createElement("div");  //Div-Element wird erstellt
+      document.body.appendChild(div);  //Div-Element wird an das Dokument drangehängt
       
-      if (i % 8 == 0) { 
+      if (i % 8 == 0) {  //Modulo 8 damit nach 8 Feldern ein Umbruch erfolgt, denn 8 % 8 = 0
             div.className += " floating";
-          Zeile++;
+          zeile++; //Das Programm soll eine Zeile hochzählen
            
       }
       
-     if (Zeile % 2 == 1) { 
+     if (zeile % 2 == 1) { //Wenn es sich um eine ungerade Zeile handelt, dann soll folgendes ausgeführt werden:
       
-      
-      if (i % 2 == 0) { 
+         //Bei einem Feld mit gerader Zahl soll mit schwarz begonnen werden und anschließend mit weiß
+         if (i % 2 == 0) {  
             div.className += " black";
-      }
-      
-      if (i % 2 == 1) { 
-            div.className += " white";
-      }
-      
          }
       
-      if (Zeile % 2 ==0){
-      
-      if (i % 2 == 0) { 
+         if (i % 2 == 1) { 
             div.className += " white";
+         }
       }
       
-      if (i % 2 == 1) { 
+      if (zeile % 2 ==0){ //Wenn es sich um eine gerade Zeile handelt, der Modulo also Null ist, dann soll folgendes ausgeführt werden:
+      
+          //Bei einem Feld mit gerader Zahl soll mit weiß begonnen werden und anschließend mit schwarz
+          if (i % 2 == 0) { 
+            div.className += " white";
+          }
+      
+          if (i % 2 == 1) { 
             div.className += " black";
-      }
-      
-    }
+          }
+       }
   
-      rice = Math.pow(2,i);
-      var riceshort: string = rice.toExponential(4);
+      //Code für Anzahl der Reiskörner
+      rice = Math.pow(2,i); //Reiskörner werden verdoppelt
+      var riceshort: string = rice.toExponential(4); //Exponentialschreibweise
       div.textContent= riceshort;
       document.body.appendChild(div);
-      }
-    
-    
+      
+   }  
  };
+
+
